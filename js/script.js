cf8eeb3568b7080735a4b2bc7d5bb733667ae799
julianyoo <julian.yoo@gmail.com>
2018-11-19 14:27:59 -0500

    loader

--- js/script.js
+++ js/script.js
@@ -12,4 +12,14 @@ $(document).ready(function(){
         $('#main').load('home.html').hide().fadeIn(1000);
     });
 
+    var myVar;
+
+    function myFunction() {
+        myVar = setTimeout(showPage, 3000);
+    }
+
+    function showPage() {
+    document.getElementById("loader").style.display = "none";
+    document.getElementById("myDiv").style.display = "block";
+    }
 });
