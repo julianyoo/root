23b76e363b2ca478b9cae220174a6757aa1872c0
julianyoo <julian.yoo@gmail.com>
2018-11-20 14:12:59 -0500

    lol

--- js/script.js
+++ js/script.js
@@ -7,6 +7,11 @@ $(document).ready(function(){
         $('#main').load('projects.html').hide().fadeIn(1000);
     });
 
+    $('#project-button').click(function(){
+        event.preventDefault();
+        $('#main').load('projects.html').hide().fadeIn(1000);
+    });
+
     $('#home').click(function(){
         event.preventDefault();
         $('#main').load('home.html').hide().fadeIn(1000);
