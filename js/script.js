b547130efca35a0069da8cdd042bcf582b26e4bb
julianyoo <julian.yoo@gmail.com>
2018-11-20 13:49:22 -0500

    main

--- js/script.js
+++ js/script.js
@@ -16,15 +16,4 @@ $(document).ready(function(){
         event.preventDefault();
         $('#main').load('home.html').hide().fadeIn(1000);
     });
-
-    var myVar;
-
-    function myFunction() {
-        myVar = setTimeout(showPage, 3000);
-    }
-
-    function showPage() {
-    document.getElementById("loader").style.display = "none";
-    document.getElementById("main").style.display = "block";
-    }
 });
